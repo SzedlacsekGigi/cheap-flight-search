a3178bece31c6b6bc6aeec4045b99c5b7a5da2e4
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2019-07-29 13:18:25 +0200

    First commit

---
+++ src/main/java/com/codecool/cheapflightsearch/CheapFlightSearchApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.cheapflightsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapFlightSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheapFlightSearchApplication.class, args);
+    }
+
+}
